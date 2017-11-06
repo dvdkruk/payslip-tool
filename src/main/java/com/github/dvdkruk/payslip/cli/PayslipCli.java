4a01c902c6d966e9db020f3aad71c31b7cbaa56b
Damiaan van der Kruk <Damiaan.van.der.Kruk@gmail.com>
2017-11-07 02:53:32 +1100

    Fixed all violations for PayslipCli.

--- src/main/java/com/github/dvdkruk/payslip/cli/PayslipCli.java
+++ src/main/java/com/github/dvdkruk/payslip/cli/PayslipCli.java
@@ -1,90 +1,166 @@
+/**
+ * Copyright (c) 2017, Damiaan van der Kruk.
+ */
 package com.github.dvdkruk.payslip.cli;
 
 import com.github.dvdkruk.payslip.PayslipProcessor;
 import com.github.dvdkruk.payslip.model.PayslipException;
 import com.github.dvdkruk.payslip.model.PayslipRequest;
 import com.github.dvdkruk.payslip.model.PayslipResult;
-
 import java.io.Console;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Command line interface class for the monthly payslip application.
+ *
+ * @author Damiaan Van Der Kruk (Damiaan.van.der.Kruk@gmail.com)
+ * @version $Id$
+ * @since 1.0
  */
-class PayslipCli {
+final class PayslipCli {
+    /**
+     * Logger.
+     */
+    private static final Logger LOG =
+        Logger.getLogger(PayslipCli.class.getName());
+    /**
+     * Input format.
+     */
+    private static final String INPUT_FORMAT =
+        "<first_name>,<last_name>,<annual_salary>,<super_rate>%,<month>";
+    /**
+     * Example input string.
+     */
+    private static final String EXAMPLE = "David,Rudd,60050,9%,March";
 
-    private static final Logger LOG = Logger.getLogger(PayslipCli.class.getName());
-
+    /**
+     * Payslip processor.
+     */
     private final PayslipProcessor processor = new PayslipProcessor();
 
+    /**
+     * System console.
+     */
     private final Console console = System.console();
 
-    private PayslipCli() {
-        if (console == null) {
+    /**
+     * Main method for starting the CLI.
+     *
+     * @param args Program arguments.
+     */
+    public static void main(final String... args) {
+        new PayslipCli().start(args);
+    }
+
+    /**
+     * Starts the program with the given args.
+     *
+     * @param args Program arguments.
+     */
+    private void start(final String... args) {
+        if (this.console == null) {
             throw new PayslipCliRuntimeException("no console");
         }
-    }
-
-    public static void main(String[] args) {
-        PayslipCli cli = new PayslipCli();
-        cli.start(args);
-    }
-
-    private void start(String[] args) {
-        if (args.length != 0) {
-            runOnce(args);
+        if (args.length == 0) {
+            this.runInteractiveMode();
         } else {
-            runInteractiveMode();
+            this.runOnce(args);
         }
     }
 
+    /**
+     * Executed the interactive mode.
+     */
     private void runInteractiveMode() {
-        console.writer().println("Employee Monthly Payslip Tool - Interactive Mode");
-        console.writer().println("Request format: <first_name>,<last_name>,<annual_salary>,<super_rate>%,<month>");
-        console.writer().println("For example: David,Rudd,60050,9%,March\n");
-
+        final PrintWriter writer = this.console.writer();
+        writer.println("Employee Monthly Payslip Tool - Interactive Mode");
+        writer.println(String
+            .format("Request format: %s", PayslipCli.INPUT_FORMAT)
+        );
+        writer.println(String.format("For example: %s%n", PayslipCli.EXAMPLE));
         boolean running = true;
         do {
-            String line = console.readLine();
-            if (line != null && !line.isEmpty()) {
+            final String line = this.console.readLine();
+            if (!isNullOrEmpty(line)) {
                 if ("exit".equals(line)) {
                     running = false;
                 } else {
-                    parse(line);
+                    this.parse(line);
                 }
             }
         } while (running);
     }
 
-    private void parse(String line) {
+    /**
+     * Checks if a string is null or empty.
+     *
+     * @param string String that needs to be checked.
+     * @return True when string is null or empty, else it returns false.
+     */
+    private static boolean isNullOrEmpty(final String string) {
+        return string == null || string.isEmpty();
+    }
+
+    /**
+     * Parses and executes the given program argument.
+     *
+     * @param line A String containing the program arguments.
+     */
+    private void parse(final String line) {
+        this.parse(line, -1);
+    }
+
+    /**
+     * Run once with the given arguments.
+     *
+     * @param args The arguments for running the program.
+     */
+    private void runOnce(final String... args) {
+        for (int index = 0; index < args.length; ++index) {
+            this.parse(args[index], index);
+        }
+    }
+
+    /**
+     * Parses and executes the given line. The index is used for given hints
+     * when to what argument is couldn't be successfully executed. When only one
+     * argument is, used index -1 or lower to disable argument number hints.
+     *
+     * @param line A String containing the program arguments.
+     * @param index Index for the argument number hint.
+     */
+    private void parse(final String line, final int index) {
         try {
-            PayslipRequest request = PayslipRequest.parse(line);
-            PayslipResult result = processor.process(request);
-            console.writer().println(result.toString());
-        } catch (PayslipException e) {
-            console.writer().println(e.getMessage());
-            LOG.log(Level.FINE, e.getMessage(), e);
-        }
-    }
-
-    private void runOnce(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            try {
-                PayslipRequest request = PayslipRequest.parse(args[i]);
-                PayslipResult result = processor.process(request);
-                console.writer().println(result.toString());
-            } catch (PayslipException e) {
-                console.writer().println("(argument: " + i + "): " + e.getMessage());
-                LOG.log(Level.FINE, "(argument: " + i + "): " + e.getMessage(), e);
+            final PayslipRequest request = PayslipRequest.parse(line);
+            final PayslipResult result = this.processor.process(request);
+            this.console.writer().println(result.toString());
+        } catch (final PayslipException pex) {
+            final String msg;
+            if (index < 0) {
+                msg = pex.getMessage();
+            } else {
+                msg = String
+                    .format("(argument: %s): %s", index, pex.getMessage());
             }
+            this.console.writer().println(msg);
+            LOG.log(Level.FINE, msg, pex);
         }
     }
 
+    /**
+     * PayslipCliRuntimeException.
+     */
     static class PayslipCliRuntimeException extends RuntimeException {
 
-        PayslipCliRuntimeException(String s) {
-            super(s);
+        /**
+         * Constructor for PayslipCliRuntimeException.
+         *
+         * @param msg Exception message.
+         */
+        PayslipCliRuntimeException(final String msg) {
+            super(msg);
         }
     }
 
