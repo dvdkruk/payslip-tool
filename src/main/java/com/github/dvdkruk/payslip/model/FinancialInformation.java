5502e56147a72c54bf29c11700774ae6bb58bc11
Damiaan van der Kruk <Damiaan.van.der.Kruk@gmail.com>
2017-11-09 19:50:56 +1100

    Fixed checkstyle for FinancialInformation.

--- src/main/java/com/github/dvdkruk/payslip/model/FinancialInformation.java
+++ src/main/java/com/github/dvdkruk/payslip/model/FinancialInformation.java
@@ -1,37 +1,81 @@
-/*
- *  Copyright (c) 2017, Damiaan van der Kruk.
- *
+/**
+ * Copyright (c) 2017, Damiaan van der Kruk.
  */
-
 package com.github.dvdkruk.payslip.model;
 
+/**
+ * Contains financial information for a month.
+ *
+ * @author Damiaan Van Der Kruk (Damiaan.van.der.Kruk@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
 public class FinancialInformation {
-    final int salary;
-    final int tax;
-    final int superannuation;
 
+    /**
+     * Monthly salary.
+     */
+    private final int salary;
+
+    /**
+     * Monthly income tax.
+     */
+    private final int tax;
+
+    /**
+     * Monthly superann sum.
+     */
+    private final int superann;
+
+    /**
+     * Financial information constructor.
+     *
+     * @param salary Monthly salary.
+     * @param tax Monthly income tax.
+     * @param superannuation Monthly superann sum.
+     */
     public FinancialInformation(
         final int salary,
         final int tax,
         final int superannuation) {
         this.salary = salary;
         this.tax = tax;
-        this.superannuation = superannuation;
+        this.superann = superannuation;
     }
 
+    /**
+     * Monthly salary.
+     *
+     * @return Monthly salary.
+     */
     final int getSalary() {
         return this.salary;
     }
 
+    /**
+     * Monthly income tax.
+     *
+     * @return Monthly income tax.
+     */
     final int getTax() {
         return this.tax;
     }
 
+    /**
+     * Monthly net income.
+     *
+     * @return Monthly net income.
+     */
     final int getNetIncome() {
         return this.salary - this.tax;
     }
 
-    public final int getSuperannuation() {
-        return this.superannuation;
+    /**
+     * Monthly superann sum.
+     *
+     * @return Monthly superann sum.
+     */
+    final int getSuperannuation() {
+        return this.superann;
     }
 }
